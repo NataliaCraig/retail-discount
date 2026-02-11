6aa25189e6b5ab0d4a080a485dd7e33c27f1c10a
Natalia <nataliacraig@usf.edu>
2026-02-11 16:00:19 -0500

    added product array

---
+++ cc_4.js
@@ -0,0 +1,33 @@
+// product array
+const products = [
+  {
+    name: "Stickers",
+    category: "Accessories",
+    price: "3.99",
+    inventory: "20",
+  },
+  {
+    name: "Bracelets",
+    category: "Accessories",
+    price: "8.99",
+    inventory: "40"
+  },
+  {
+    name: "Sunscreen",
+    category: "Skincare",
+    price: "16.99",
+    inventory: "15"
+  },
+  {
+    name: "Perfume",
+    category: "Fragrance",
+    price: "30.00",
+    inventory: "20"
+},
+{
+    name: "Lotion",
+    category: "Skincare",
+    price: "20.99",
+    inventory: "12"
+}
+];
